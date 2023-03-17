97166744c881912292e7df7554aa8dfd69ddda58
Nirmal Shrestha <nirmal.work01@gmail.com>
2023-03-17 15:04:38 +0545

    Logic Building - Set up highest score as per our guess

--- script.js
+++ script.js
@@ -18,10 +18,21 @@ document.querySelector('.guess').value = 15;
 // if guess number is below secrect number then display Too Low.
 // if guess number is above secrect number then display Too High.
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.number').textContent = secretNumber;
+// Second Phase:
+// If our every wrong guess value of score need to be deducted by 1.
+// if score become 0 then display message "You lost the game!"
 
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
 const message = document.querySelector('.message');
+const highestScore = document.querySelector('.score');
+let score = 20;
+
+
+
+
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.score').textContent = score;
 
 document.querySelector('.check').addEventListener('click' , function() {
     const guess  = Number(document.querySelector('.guess').value);
@@ -33,9 +44,26 @@ document.querySelector('.check').addEventListener('click' , function() {
         message.textContent = "🍾 Correct Answer!";
     }
     else if(guess < secretNumber) {
+
+        if(score > 1) {
         message.textContent = "📉 Too Low!";
+        score--;
+        highestScore.textContent = score;
+        }
+        else {
+            message.textContent = "You Lost The Game!";
+            highestScore.textContent = 0;
+        }
+        
     }
     else if(guess > secretNumber) {
-        message.textContent = "📈 Too High!";
+        if(score > 1) {
+            message.textContent = "📈 Too High!";
+            score--;
+            highestScore.textContent = score;
+        }else {
+            message.textContent = "You Lost The Game!";
+            highestScore.textContent = 0;
+        }      
     }
 } )
